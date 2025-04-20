2f25bb8f41d5a4c1217795ef7254cf665ea9d289
TylerWerne40 <TylerWerne40@gmail.com>
2025-04-20 17:11:53 -0600

    that wasn't run

--- micro_blog/app.py
+++ micro_blog/app.py
@@ -23,7 +23,7 @@ def create_app(config_object="micro_blog.settings"):
 
     :param config_object: The configuration object to use.
     """
-    app = Flask(__name__.split(".")[0], debug=True)
+    app = Flask(__name__.split(".")[0])
     app.config.from_object(config_object)
     register_extensions(app)
     register_blueprints(app)
